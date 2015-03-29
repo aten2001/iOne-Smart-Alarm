ac02c67cf1aa25c3e122abcb04a60f1c2e6fecf8
Hyun Seo Chung <hchung48@gatech.edu>
2015-03-29 02:35:49 -0400

    Added PebbleController class to use to communicate with the Pebble watch. Added basic functions and placeholders to send/receive data to a watch and start/stop the app on a watch.

---
+++ app/src/main/java/com/team8/ionesmartalarm/PebbleController.java
@@ -0,0 +1,50 @@
+package com.team8.ionesmartalarm;
+
+import android.content.Context;
+import android.os.Handler;
+
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+import java.util.UUID;
+
+public class PebbleController {
+
+    private static final UUID ALARM_UUID = UUID.fromString("?????"); // TODO: replace with the actual UUID of the app
+    private PebbleKit.PebbleDataReceiver alarmDataReceiver = null;
+
+    public void beginReceivingDataFromWatch(Context context) {
+        final Handler handler = new Handler();
+        alarmDataReceiver = new PebbleKit.PebbleDataReceiver(ALARM_UUID) {
+            @Override
+            public void receiveData(Context context, int transactionId, PebbleDictionary pebbleTuples) {
+                PebbleKit.sendAckToPebble(context, transactionId);
+                // TODO: handle received data
+            }
+        };
+        PebbleKit.registerReceivedDataHandler(context, alarmDataReceiver);
+    }
+
+    public void stopReceivingDataFromWatch(Context context) {
+        if (alarmDataReceiver != null) {
+            context.unregisterReceiver(alarmDataReceiver);
+            alarmDataReceiver = null;
+        }
+    }
+
+    public void sendDataToWatch(Context context) {
+        PebbleDictionary data = new PebbleDictionary();
+
+        // TODO: add some stuff to the data
+
+        PebbleKit.sendDataToPebble(context, ALARM_UUID, data);
+    }
+
+    public void startAlarmApp(Context context) {
+        PebbleKit.startAppOnPebble(context, ALARM_UUID);
+    }
+
+    public void stopAlarmApp(Context context) {
+        PebbleKit.closeAppOnPebble(context, ALARM_UUID);
+    }
+}
